b83c37c7283644caa2c1cd86a8ff2deb073e8cb8
Steve Goldman <gutezach@gmail.com>
2018-03-14 16:56:22 -0400

    Added Binary Numbers

---
+++ src/BinaryNumbers.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QLChallenge
+{
+    class BinaryNumbers
+    {
+        private static int[] array;
+
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string binary = Convert.ToString(n, 2);
+            char [] binaryArray = binary.ToCharArray();
+            int oneCounter = 0;
+            int finalOneCounter = 0;
+            for(int i = 0; i < binaryArray.Length; i++)
+            {
+                if(binaryArray[i] == '1')
+                {
+                    oneCounter++;
+                    if(oneCounter > finalOneCounter)
+                    {
+                        finalOneCounter = oneCounter;
+                    }
+                }
+                else
+                {
+                    oneCounter = 0;
+                }
+            }
+            Console.WriteLine(finalOneCounter);
+        }
+    }
+}
