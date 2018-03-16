b41d0ddd45c991e5e6384c6fd5157414a4bcef60
Steve Goldman <gutezach@gmail.com>
2018-03-16 12:55:17 -0400

    Adding staircase challenge

---
+++ src/Staircase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace HackerRankChallenge
+{
+    // Print a staircase of any given size
+    class Staircase
+    {
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            staircase(n);
+        }
+
+        static void staircase(int n)
+        {
+            StringBuilder staircase = new StringBuilder(new string(' ', n));
+            for(int i = staircase.Length-1; i >= 0; i--)
+            {
+                staircase[i] = '#';
+                Console.WriteLine(staircase);
+            }
+        }
+    }
+}
